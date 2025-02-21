afd545063a94e79ca331892ed3fbe706ed06cee8
Uli Kusterer <ulivc@zathras.de>
2025-02-21 21:56:57 +0100

    Make it clearer buf is an implementation detail, and add two more insert methods.

--- cow_str/cow_str.cpp
+++ cow_str/cow_str.cpp
@@ -6,8 +6,8 @@
 using namespace cow;
 using namespace std;
 
-char buf::empty_cstr{'\0'};
-buf buf::_empty_buf;
+char str::buf::empty_cstr{'\0'};
+str::buf str::buf::_empty_buf;
 
 
 cow::str operator ""_cow (const char* inLiteralStr, unsigned long) {
@@ -43,3 +43,37 @@ void str::insert(iterator dest, const str& inNewEnd) {
 	}
 	oldBuf->release();
 }
+
+void str::insert(iterator dest, size_t count, char ch) {
+	if (count == 0) { return; }
+	size_t offset = distance(_buf->data(), dest);
+	auto oldBuf = _buf;
+	_buf = new buf(_buf->length() + count, ch);
+	if (offset > 0) {
+		memcpy(_buf->data(), oldBuf->data(), offset);
+	}
+	// Skip newly-inserted characters, they're already initialized to the right value.
+	size_t afterInsertionOffset = offset + count;
+	if (afterInsertionOffset < _buf->length()) {
+		memcpy(_buf->data() + afterInsertionOffset, oldBuf->data() + offset, oldBuf->length() - offset);
+	}
+	oldBuf->release();
+}
+
+void str::insert(iterator dest, const_iterator beg, const_iterator ed) {
+	size_t count = distance(beg, ed);
+	if (count == 0) { return; }
+	size_t offset = distance(_buf->data(), dest);
+	auto oldBuf = _buf;
+	_buf = new buf(_buf->length() + count, '\0');
+	if (offset > 0) {
+		memcpy(_buf->data(), oldBuf->data(), offset);
+	}
+	memcpy(_buf->data() + offset, beg, count);
+	size_t afterInsertionOffset = offset + count;
+	if (afterInsertionOffset < _buf->length()) {
+		memcpy(_buf->data() + afterInsertionOffset, oldBuf->data() + offset, oldBuf->length() - offset);
+	}
+	oldBuf->release();
+}
+
--- cow_str/cow_str.hpp
+++ cow_str/cow_str.hpp
@@ -11,76 +11,81 @@
 namespace cow {
 	
 	using namespace std;
-	
-	/*! buf is a refcounted string storage. It always allocates a nul terminator for cheap
-		conversion to C-string. The refcount is guaranteed to be thread-safe, for write
-		access you must be exclusive owner of a buf, or you will not be thread-safe. */
-	class buf {
-	public:
-		enum is_constant {
-			not_constant,
-			constant
-		};
 		
-		buf() : _bytes{&empty_cstr}, _len{0}, _isconstant{true} {}
-		buf(const char* str, is_constant isc) : _len(strlen(str)), _isconstant{isc == constant} { if (_isconstant) { _bytes = const_cast<char*>(str); } else { _bytes = (char*) calloc(_len + 1, 1); memcpy(_bytes, str, _len); } }
-		buf(const char* str, size_t len) : _bytes((char*)calloc(len + 1, 1)), _len(len) { memcpy(_bytes, str, _len); }
-		buf(size_t count, char ch) : _bytes((char*)calloc(count + 1, 1)), _len(count) { memset(_bytes, ch, _len); }
-		buf(const buf&) = delete;
-		
-		~buf() {
-			if (!_isconstant) {
-				free(_bytes);
-			}
-			_bytes = nullptr;
-			_isconstant = true;
-			_len = 0;
-		}
-		
-		/*! Copy buf if caller is not the only owner.
-			Gives up ownership of buf if it gives you a new copy. */
-		buf* mutableCopy() {
-			if (_refcount == 1) {
-				return this;
-			} else {
-				auto newBuf = new buf(_bytes, _len);
-				release();
-				return newBuf;
-			}
-		}
-		//! Increment refcount to make us co-owner of buf.
-		buf* retain() { ++_refcount; return this; };
-		//! Give up caller's ownership of buf, deleting it of caller was last owner.
-		void release() { if (--_refcount == 0) { delete this; } }
-
-		const char* data() const { return _bytes; };
-		char* data() { return _bytes; };
-		
-		size_t length() { return _len; }
-		
-		static buf* empty_buf() { return _empty_buf.retain(); }
-		
-	private:
-		static char empty_cstr;
-		static buf _empty_buf;
-		
-		char *_bytes{nullptr};
-		size_t _len : 63{0};
-		size_t _isconstant : 1{false};
-		atomic<size_t> _refcount{1};
-	};
-	static_assert(sizeof(buf) == sizeof(char*) + sizeof(size_t) + sizeof(size_t), "Uses minimal space");
-	
 	/*! str guarantees:
 		- _buf is lazily copied on write.
 		- only calls that mutate _buf invalidate iterators. (non-const [] and at() are considered mutation)
 		- default constructed strings do not incur an additional buf allocation.
 		- constant strings constructed with ""_cow will not copy their string until actually mutated.
 		- limited thread-safety: copies of strings can be moved between threads as they will only read-access their buf (and will copy it on write access)
+		
+		Note that str is not a template class, as it is assumed that every sensible
+		app these days uses UTF-8, and it's therefore unnecessary to provide additional
+		character widths.
 	 */
 	class str {
 	public:
-		using is_constant = buf::is_constant;
+		enum is_constant {
+			not_constant,
+			constant
+		};
+		
+	private:
+		/*! buf is a refcounted string storage. It always allocates a nul terminator for cheap
+			conversion to C-string. The refcount is guaranteed to be thread-safe, for write
+			access you must be exclusive owner of a buf, or you will not be thread-safe. */
+		class buf {
+		public:
+			buf() : _bytes{&empty_cstr}, _len{0}, _isconstant{true} {}
+			buf(const char* str, is_constant isc) : _len(strlen(str)), _isconstant{isc == constant} { if (_isconstant) { _bytes = const_cast<char*>(str); } else { _bytes = (char*) calloc(_len + 1, 1); memcpy(_bytes, str, _len); } }
+			buf(const char* str, size_t len) : _bytes((char*)calloc(len + 1, 1)), _len(len) { memcpy(_bytes, str, _len); }
+			buf(size_t count, char ch) : _bytes((char*)calloc(count + 1, 1)), _len(count) { memset(_bytes, ch, _len); }
+			buf(const buf&) = delete;
+			
+			~buf() {
+				if (!_isconstant) {
+					free(_bytes);
+				}
+				_bytes = nullptr;
+				_isconstant = true;
+				_len = 0;
+			}
+			
+			/*! Copy buf if caller is not the only owner.
+				Gives up ownership of buf if it gives you a new copy. */
+			buf* mutableCopy() {
+				if (_refcount == 1) {
+					return this;
+				} else {
+					auto newBuf = new buf(_bytes, _len);
+					release();
+					return newBuf;
+				}
+			}
+			//! Increment refcount to make us co-owner of buf.
+			buf* retain() { ++_refcount; return this; };
+			//! Give up caller's ownership of buf, deleting it of caller was last owner.
+			void release() { if (--_refcount == 0) { delete this; } }
+
+			const char* data() const { return _bytes; };
+			char* data() { return _bytes; };
+			
+			size_t length() { return _len; }
+			
+			static buf* empty_buf() { return _empty_buf.retain(); }
+			
+		private:
+			static char empty_cstr;
+			static buf _empty_buf;
+			
+			char *_bytes{nullptr};
+			size_t _len : 63{0};
+			size_t _isconstant : 1{false};
+			atomic<size_t> _refcount{1};
+		};
+		static_assert(sizeof(buf) == sizeof(char*) + sizeof(size_t) + sizeof(size_t), "Uses minimal space");
+
+	public:
 		using const_iterator = const char*;
 		using iterator = char*;
 
@@ -114,6 +119,8 @@ namespace cow {
 		void append(const str& inNewEnd);
 		
 		void insert(iterator dest, const str& inNewEnd);
+		void insert(iterator dest, size_t count, char ch);
+		void insert(iterator dest, const_iterator beg, const_iterator ed);
 
 	private:
 		buf *_buf;
